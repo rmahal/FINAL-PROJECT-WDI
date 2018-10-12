81127cadef142b3b3d6c38ee69c43fbbe25bc9ff
rajmahal <rmahal25@gmail.com>
2018-10-12 12:31:21 -0700

    Backends are playing nicely

--- back/server.js
+++ back/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const parser = require('body-parser')
-
-
+const ejs = require('ejs')
+const cors = require('cors');
+const axios = require('axios');
+const fetch = require("node-fetch");
 const app = express()
-
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(cors())
 
 app.use(express.static('public'));
 
@@ -21,6 +24,20 @@ app.get("/", (req,res)=>{
 app.get("/org", (req,res)=>{
     res.sendFile('views/orgchart.html', {root: __dirname});
 })
+
+
+app.get('/userprofile/:id', function(req, res) {
+    var id = req.params.id;
+    console.log("ID: "+id)
+
+    fetch('http://localhost:3001/employee/'+id)
+    .then(res => res.json())
+    .then(function(response) {
+        console.log(response)
+    })
+
+});
+
 app.get("/userprofile", (req,res)=>{
     res.sendFile('views/userprofile.html', {root: __dirname});
 })
--- hr/server.js
+++ hr/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const parser = require('body-parser')
 const db = require('./models');
+const cors = require('cors');
 const app = express()
 
 app.use(function(req, res, next) {
@@ -8,7 +9,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
+app.use(cors())
 
 app.get("/allemployees", (req,res)=>{
     
@@ -22,10 +23,21 @@ app.get("/allemployees", (req,res)=>{
 })
 
 
+app.get("/employee/:id", (req,res)=>{
+
+    db.Employee.find({_id: req.params.id}, (err, uFound) => {
+        if(err){
+            res.status(401);
+        }else{
+            res.json(uFound)
+        }
+    })
+})
+
+
 
 app.get("/employee/manager/populate/:id", (req,res)=>{
     
-
     db.Employee.find({_id: req.params.id}, (err, uFound) => {
         if(err){
             res.status(401);
@@ -42,7 +54,6 @@ app.get("/employee/manager/populate/:id", (req,res)=>{
 
 app.get("/employee/hr/populate/:id", (req,res)=>{
     
-
     db.Employee.find({_id: req.params.id}, (err, uFound) => {
         if(err){
             res.status(401);
